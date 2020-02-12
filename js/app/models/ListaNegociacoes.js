9088b448b3be4863c27a71ab9be2aa2344d9aa41
Nathan José Rezini <nathanrezini@gmail.com>
2020-02-11 23:42:31 -0300

    Adicionado um proxy em lista negociacoes

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,26 +1,19 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
-        
-        this._negociacoes = [];
-        this._armadilha = armadilha;
+    constructor() {
+  
+          this._negociacoes = [];
     }
-
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        
     }
-
+  
     get negociacoes() {
-        
         return [].concat(this._negociacoes);
     }
-
+  
     esvazia() {
-        
         this._negociacoes = [];
-        this._armadilha(this);
     }
-}
+  }
